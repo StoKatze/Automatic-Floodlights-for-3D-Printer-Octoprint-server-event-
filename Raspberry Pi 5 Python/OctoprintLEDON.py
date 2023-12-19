aad456db9cb0029d98dc38bbeadd2273ff3bea4a
Christian V <44880451+StoKatze@users.noreply.github.com>
2023-12-19 22:36:54 +0100

    Add files via upload

---
+++ Raspberry Pi 5 Python/OctoprintLEDON.py
@@ -0,0 +1,21 @@
+import gpiod
+from gpiod.line import Direction, Value
+
+# Constants
+# GPIO
+# Chip
+CHIP = "/dev/gpiochip4"
+# Relay signal pin
+PIN_IMP = 4
+
+# Actual program
+# Relay on
+with gpiod.request_lines(CHIP, consumer="RELAY",
+    config={
+        PIN_IMP: gpiod.LineSettings(
+            # Set as Output
+            direction=Direction.OUTPUT, output_value=Value.ACTIVE
+        )
+    },
+) as request:
+     request.set_value(PIN_IMP, Value.ACTIVE)
